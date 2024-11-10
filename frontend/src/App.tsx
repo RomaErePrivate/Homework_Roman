131fd855e4c34f38ef7800d9cc2db19f8d278630
roremin <reremin@cisco.com>
2024-11-10 10:16:52 +0200

    FE refactor

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -7,7 +7,7 @@ const App = () => {
   return (
       <Router>
         <Routes>
-            <Route path="/login" element={<LoginPage />} />
+            <Route path="/" element={<LoginPage />} />
             <Route path="/dashboard" element={<DashboardPage />} />
         </Routes>
       </Router>
--- frontend/src/axiosConfig.ts
+++ frontend/src/axiosConfig.ts
@@ -1,5 +1,6 @@
 import axios from 'axios';
 
-axios.defaults.baseURL = 'http://localhost:5001';
+// axios.defaults.baseURL = 'http://localhost:5001';
+axios.defaults.baseURL = 'http://35.157.168.67:5001';
 axios.defaults.headers.post['Content-Type'] = 'application/json';
 export default axios;
